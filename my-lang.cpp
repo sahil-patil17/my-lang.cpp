83dcc6e0ea2692b2a63ed30533d4974abbbf03f4
B3nzeneKnight <sahilpatil601@gmail.com>
2023-08-10 01:26:13 +0530

    Third commit

--- my-lang.cpp
+++ my-lang.cpp
@@ -17,3 +17,71 @@ enum Token
 
 static std::string IdentifierStr; // Filled in if parser finds a string tok_identifier
 static double NumVal;             // Filled in if parser finds a number tok_number
+
+/// gettok - Return the next token from standard input.
+static int gettok()
+{
+    static int LastChar = ' ';
+
+    // Skip any whitespace.
+    while (isspace(LastChar))
+    {
+        LastChar = getchar(); // getchar() reads a single character that you have typed in & stores it.
+    }
+
+    // 2) RECOGNIZING IDENTIFIERS
+
+    if (isalpha(LastChar))
+    {
+        // identifier: [a-zA-Z][a-zA-Z0-9]*
+
+        IdentifierStr = LastChar;
+
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        // IF THE IDENTIFIER HAS 2 OF THE VARIABLES MENTIONED IN OUR LANGUAGE, CONVERT THEM TO TOKENS.
+        if (IdentifierStr == "def")
+            return tok_def; //
+        if (IdentifierStr == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+
+    // 3) RECOGNIZING DIGITS
+    if (isdigit(LastChar) || LastChar == '.')
+    { // Number: [0-9.]+
+        std::string NumStr;
+        do
+        {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    // 4) RECOGNIZING COMMENTS
+    if (LastChar == '#')
+    {
+        // Comment until end of line.
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok(); // Recursive call
+    }
+
+    // Check for end of file.  Don't eat the EOF.
+    if (LastChar == EOF)
+    {
+        return tok_eof;
+    }
+
+    // Otherwise, just return the character as its ascii value.
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
